9aff99006b0e4b2e2d2776f4e73163a2357ae904
Claudio Henrique <claudio_henrique20@hotmail.com>
2023-05-19 17:36:54 -0300

    environments and profiles

--- src/main/java/io/github/cHenrique0/IntroConfiguration.java
+++ src/main/java/io/github/cHenrique0/IntroConfiguration.java
@@ -1,8 +1,18 @@
 package io.github.cHenrique0;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development") /* Faz a configuração ficar disponivel apenas no ambiente passado por parametro*/
 public class IntroConfiguration {
 
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("> RODANDO A CONFIGURAÇÃO DO AMBIENTE DE DESENVOLVIMENTO");
+        };
+    }
 }
