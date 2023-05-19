d5be190a0a502ead6179205016640e423ca76d9a
Claudio Henrique <claudio_henrique20@hotmail.com>
2023-05-19 15:56:06 -0300

    application properties

--- src/main/java/io/github/cHenrique0/IntroApplication.java
+++ src/main/java/io/github/cHenrique0/IntroApplication.java
@@ -2,6 +2,7 @@ package io.github.cHenrique0;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IntroApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}") // chave no arquivo 'application.properties'
     private String applicationName;
 
     @GetMapping("/hello")
--- src/main/java/io/github/cHenrique0/IntroConfiguration.java
+++ src/main/java/io/github/cHenrique0/IntroConfiguration.java
@@ -1,13 +1,8 @@
 package io.github.cHenrique0;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class IntroConfiguration {
 
-    @Bean(name="applicationName")
-    public String applicationName() {
-        return "Introdução ao Spring Boot";
-    }
 }
