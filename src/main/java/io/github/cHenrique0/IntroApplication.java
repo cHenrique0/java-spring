8c087a633dae5c892aad4e65c2a295266eb34a2f
Claudio Henrique <claudio_henrique20@hotmail.com>
2023-05-23 11:18:08 -0300

    update, delete and select specific

--- src/main/java/io/github/cHenrique0/IntroApplication.java
+++ src/main/java/io/github/cHenrique0/IntroApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class IntroApplication {
 
@@ -16,14 +18,40 @@ public class IntroApplication {
     public CommandLineRunner init(@Autowired ClienteRepository repository) {
         return args -> {
             // Salva no banco
-            /*Cliente cliente1 = new Cliente("Cliente teste 1");
-            repository.salvar(cliente1);
-
-            Cliente cliente2 = new Cliente("Cliente teste 2");
-            repository.salvar(cliente2);*/
+            System.out.println("\n/===== Inserindo dados =====/");
+            repository.salvar(new Cliente("Cliente teste 1"));
+            repository.salvar(new Cliente("Cliente teste 2"));
+            repository.salvar(new Cliente("Outro cliente 1"));
+            repository.salvar(new Cliente("Outro cliente 2"));
 
             // Lista os dados do banco
-            repository.listarTodos().forEach(System.out::println);
+            List<Cliente> clientes = repository.listarTodos();
+            clientes.forEach(System.out::println);
+
+            // Atualiza os dados
+            System.out.println("\n/===== Atualizando dados =====/");
+            clientes.forEach(cliente -> {
+                cliente.setNome(cliente.getNome() + " atualizado");
+                repository.atualizar(cliente);
+            });
+            clientes = repository.listarTodos();
+            clientes.forEach(System.out::println);
+
+            // Lista dados por nome
+            System.out.println("\n/===== Buscando por dados especificos =====/");
+            List<Cliente> buscaPorNome = repository.buscarPorNome("teste");
+            buscaPorNome.forEach(System.out::println);
+
+            // Deleta uma dado
+            System.out.println("\n/===== Deletando todos dados =====/");
+            clientes.forEach(repository::deletar);
+
+            System.out.println("\n/===== Listando os dados após as operações =====/");
+            clientes = repository.listarTodos();
+            if (clientes.isEmpty()) {
+                System.out.println("> Nenhum cliente encontrado");
+            }
+            clientes.forEach(System.out::println);
         };
     }
 
--- src/main/java/io/github/cHenrique0/domain/repository/ClienteRepository.java
+++ src/main/java/io/github/cHenrique0/domain/repository/ClienteRepository.java
@@ -16,6 +16,9 @@ public class ClienteRepository {
     /* Scripts */
     private static final String INSERT = "insert into cliente (nome) values (?)";
     private static final String SELECT_ALL = "select * from cliente";
+    private static final String UPDATE = "update cliente set nome = ? where id = ?";
+    private static final String DELETE = "delete from cliente where id = ?";
+    private static final String SELECT_BY_NAME = "select * from cliente where nome like ?";
 
     /* Conexão com o banco configurado em application.properties */
     @Autowired
@@ -26,7 +29,27 @@ public class ClienteRepository {
     }
 
     public List<Cliente> listarTodos() {
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+        return jdbcTemplate.query(SELECT_ALL, getClienteRowMapper());
+    }
+
+    public List<Cliente> buscarPorNome(String nome){
+        return jdbcTemplate.query(SELECT_BY_NAME, new Object[]{"%" + nome + "%"}, getClienteRowMapper());
+    }
+
+    public void atualizar(Cliente cliente) {
+        jdbcTemplate.update(UPDATE, cliente.getNome(), cliente.getId());
+    }
+
+    public void deletar(Cliente cliente){
+        this.deletar(cliente.getId());
+    }
+
+    public void deletar(int id){
+        jdbcTemplate.update(DELETE, id);
+    }
+
+    private static RowMapper<Cliente> getClienteRowMapper() {
+        return new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
 
@@ -35,6 +58,6 @@ public class ClienteRepository {
 
                 return new Cliente(id, nome);
             }
-        });
+        };
     }
 }
